29bb3d8ac8b469e18236345b1b17148ea819b8a9
Ted M. Young <tedyoung@gmail.com>
2022-01-25 11:35:10 -0800

    LAB #3: Extracted Suit as an enum, cleaned up tests to clarify "dummy suit".

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -2,31 +2,39 @@ package com.jitterted.ebp.blackjack;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class HandValueAceTest {
 
+    private static final Suit DUMMY_SUIT = Suit.SPADES;
+
     @Test
     public void handWithOneAceTwoCardsIsValuedAt11() throws Exception {
         Game game = new Game();
-        List<Card> cards = List.of(new Card("whocares", "A"),
-                                   new Card("whocares", "5"));
+        List<Card> cards = createCardListOf("A", "5");
 
         assertThat(game.handValueOf(cards))
-                .isEqualTo(11 + 5);
+                .isEqualTo(11 + 5); // EVIDENT DATA
     }
 
     @Test
     public void handWithOneAceAndOtherCardsEqualTo11IsValuedAt1() throws Exception {
         Game game = new Game();
-        List<Card> cards = List.of(new Card("whocares", "A"),
-                                   new Card("whocares", "8"),
-                                   new Card("whocares", "3"));
+        List<Card> cards = createCardListOf("A", "8", "3");
 
         assertThat(game.handValueOf(cards))
                 .isEqualTo(1 + 8 + 3);
+    }
+
+    private List<Card> createCardListOf(String... ranks) {
+        return Arrays.stream(ranks)
+                     .map(rank -> new Card(DUMMY_SUIT, rank))
+                     .collect(Collectors.toList());
     }
 
 }
