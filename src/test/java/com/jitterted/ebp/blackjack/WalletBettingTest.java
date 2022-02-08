5037fb282be6f278aecf483884ee16b9eefa27a4
Ted M. Young <tedyoung@gmail.com>
2022-02-08 11:17:00 -0800

    LAB #8: Betting full amount causes isEmpty to be true.

--- src/test/java/com/jitterted/ebp/blackjack/WalletBettingTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletBettingTest.java
@@ -8,8 +8,7 @@ public class WalletBettingTest {
 
     @Test
     public void walletWith12BalanceBet8ThenBalanceIs4() throws Exception {
-        Wallet wallet = new Wallet();
-        wallet.addMoney(12);
+        Wallet wallet = createWalletWith(12);
 
         wallet.bet(8);
 
@@ -19,8 +18,7 @@ public class WalletBettingTest {
 
     @Test
     public void walletWith27Bet7AndBet9ThenBalanceIs11() throws Exception {
-        Wallet wallet = new Wallet();
-        wallet.addMoney(27);
+        Wallet wallet = createWalletWith(27);
 
         wallet.bet(7);
         wallet.bet(9);
@@ -29,4 +27,23 @@ public class WalletBettingTest {
                 .isEqualTo(27 - 7 - 9);
     }
 
+    @Test
+    public void betFullBalanceThenWalletIsEmpty() throws Exception {
+        Wallet wallet = createWalletWith(73);
+
+        wallet.bet(73);
+
+        assertThat(wallet.isEmpty())
+                .isTrue();
+    }
+
+    private Wallet createWalletWith(int initialAmount) {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(initialAmount);
+        return wallet;
+    }
+
+
 }
+
+
