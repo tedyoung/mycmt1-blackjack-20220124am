d7a46015b24bbe89a597f05953259ee952ffc176
Ted M. Young <tedyoung@gmail.com>
2022-02-08 10:11:02 -0800

    TDD: Add money once and multiple times updates balance.

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -37,7 +37,26 @@ public class WalletTest {
                 .isZero();
     }
 
+    @Test
+    public void newWalletAdd10ThenBalanceIs10() throws Exception {
+        Wallet wallet = new Wallet();
 
+        wallet.addMoney(10);
+
+        assertThat(wallet.balance())
+                .isEqualTo(10);
+    }
+
+    @Test
+    public void newWalletAdd7AndAdd8ThenBalanceIs15() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(7);
+        wallet.addMoney(8);
+
+        assertThat(wallet.balance())
+                .isEqualTo(7 + 8);
+    }
 
 }
 
